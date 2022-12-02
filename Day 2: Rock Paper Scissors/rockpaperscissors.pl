a05e2b25da8465826cd520f593fb04b587062729
Spencer Comin <scomin@me.com>
2022-12-02 06:03:17 -0500

    Day 2 Part 2

--- Day 2: Rock Paper Scissors/rockpaperscissors.pl
+++ Day 2: Rock Paper Scissors/rockpaperscissors.pl
@@ -1,7 +1,9 @@
-solution(F, X) :-
+solution(F, X, Y) :-
     file_lines(F, Lines),
-    maplist(points, Lines, Points),
-    sum_list(Points, X).
+    maplist(points1, Lines, Points1),
+    sum_list(Points1, X),
+    maplist(points2, Lines, Points2),
+    sum_list(Points2, Y).
 
 file_lines(File, Lines) :-
     setup_call_cleanup(open(File, read, In),
@@ -12,12 +14,22 @@ stream_lines(In, Lines) :-
     read_string(In, _, Str),
     split_string(Str, "\n", "", Lines).
 
-points("A X", 1+3).
-points("A Y", 2+6).
-points("A Z", 3+0).
-points("B X", 1+0).
-points("B Y", 2+3).
-points("B Z", 3+6).
-points("C X", 1+6).
-points("C Y", 2+0).
-points("C Z", 3+3).
+points1("A X", 1+3).
+points1("A Y", 2+6).
+points1("A Z", 3+0).
+points1("B X", 1+0).
+points1("B Y", 2+3).
+points1("B Z", 3+6).
+points1("C X", 1+6).
+points1("C Y", 2+0).
+points1("C Z", 3+3).
+
+points2("A X", 3+0).
+points2("A Y", 1+3).
+points2("A Z", 2+6).
+points2("B X", 1+0).
+points2("B Y", 2+3).
+points2("B Z", 3+6).
+points2("C X", 2+0).
+points2("C Y", 3+3).
+points2("C Z", 1+6).
